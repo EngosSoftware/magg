745990c75a76b8c12022ffe32ff2768f5d7d50dc
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-01-15 10:48:25 +0100

    Updates.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 ### magg
 
 [![crates.io][crates-badge]][crates-url]
-[![Code coverage][cov-badge]][cov-url]
+[![coverage][cov-badge]][cov-url]  
+![build Linux][build-badge-linux]
+![build Windows][build-badge-windows]
+![build macOs][build-badge-macos]
+![build macOs arm64][build-badge-macos-arm64]
 
 [crates-badge]: https://img.shields.io/crates/v/magg.svg
 [crates-url]: https://crates.io/crates/magg
 [cov-badge]: https://img.shields.io/badge/coverage-0%25-21b577.svg
 [cov-url]: https://crates.io/crates/coverio
+[build-badge-linux]: https://github.com/EngosSoftware/magg/actions/workflows/build-linux.yml/badge.svg
+[build-badge-windows]: https://github.com/EngosSoftware/magg/actions/workflows/build-windows.yml/badge.svg
+[build-badge-macos]: https://github.com/EngosSoftware/magg/actions/workflows/build-macos.yml/badge.svg
+[build-badge-macos-arm64]: https://github.com/EngosSoftware/magg/actions/workflows/build-macos-arm64.yml/badge.svg
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,29 @@ fn get_package_name(parsed: &toml::Value) -> &str {
   parsed["package"]["name"].as_str().expect("package.name not found in Cargo.toml")
 }
 
+fn get_repository(parsed: &toml::Value) -> &str {
+  parsed["package"]["repository"].as_str().expect("package.repository not found in Cargo.toml")
+}
+
 fn main() {
   let parsed_toml = parse_toml("Cargo.toml");
   let package_name = get_package_name(&parsed_toml);
+  let repository = get_repository(&parsed_toml).strip_suffix(".git").expect("repository name does not end with '.git'");
   println!("### {}", package_name);
   println!();
   println!("[![crates.io][crates-badge]][crates-url]");
-  println!("[![Code coverage][cov-badge]][cov-url]");
+  println!("[![coverage][cov-badge]][cov-url]  ");
+  println!("![build Linux][build-badge-linux]");
+  println!("![build Windows][build-badge-windows]");
+  println!("![build macOs][build-badge-macos]");
+  println!("![build macOs arm64][build-badge-macos-arm64]");
   println!();
   println!("[crates-badge]: https://img.shields.io/crates/v/{package_name}.svg");
   println!("[crates-url]: https://crates.io/crates/{package_name}");
   println!("[cov-badge]: https://img.shields.io/badge/coverage-0%25-21b577.svg");
   println!("[cov-url]: https://crates.io/crates/coverio");
+  println!("[build-badge-linux]: {repository}/actions/workflows/build-linux.yml/badge.svg");
+  println!("[build-badge-windows]: {repository}/actions/workflows/build-windows.yml/badge.svg");
+  println!("[build-badge-macos]: {repository}/actions/workflows/build-macos.yml/badge.svg");
+  println!("[build-badge-macos-arm64]: {repository}/actions/workflows/build-macos-arm64.yml/badge.svg");
 }

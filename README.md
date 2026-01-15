f450c412f281055cf1b057238e77c4d21302378f
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-01-15 10:39:16 +0100

    Updates.

---
+++ README.md
@@ -0,0 +1,9 @@
+### magg
+
+[![crates.io][crates-badge]][crates-url]
+[![Code coverage][cov-badge]][cov-url]
+
+[crates-badge]: https://img.shields.io/crates/v/magg.svg
+[crates-url]: https://crates.io/crates/magg
+[cov-badge]: https://img.shields.io/badge/coverage-0%25-21b577.svg
+[cov-url]: https://crates.io/crates/coverio
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use std::path::Path;
+
+fn parse_toml(file_name: impl AsRef<Path>) -> toml::Value {
+  let content = fs::read_to_string(file_name).expect("failed to read TOML file");
+  toml::from_str(&content).expect("failed to parse TOML file")
+}
+
+fn get_package_name(parsed: &toml::Value) -> &str {
+  parsed["package"]["name"].as_str().expect("package.name not found in Cargo.toml")
+}
+
 fn main() {
-    println!("Hello, magic world!");
+  let parsed_toml = parse_toml("Cargo.toml");
+  let package_name = get_package_name(&parsed_toml);
+  println!("### {}", package_name);
+  println!();
+  println!("[![crates.io][crates-badge]][crates-url]");
+  println!("[![Code coverage][cov-badge]][cov-url]");
+  println!();
+  println!("[crates-badge]: https://img.shields.io/crates/v/{package_name}.svg");
+  println!("[crates-url]: https://crates.io/crates/{package_name}");
+  println!("[cov-badge]: https://img.shields.io/badge/coverage-0%25-21b577.svg");
+  println!("[cov-url]: https://crates.io/crates/coverio");
 }

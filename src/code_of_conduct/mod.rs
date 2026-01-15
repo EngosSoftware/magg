8ac9f742ff95ff525213d6877140fecf2c5b058d
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-01-15 18:55:45 +0100

    Updates.

--- src/code_of_conduct/mod.rs
+++ src/code_of_conduct/mod.rs
@@ -1,6 +1,6 @@
 //! Code of conduct generator
 
-const CODE_OF_CONDUCT: &str = include_str!("CODE_OF_CONDUCT.md");
+const CODE_OF_CONDUCT: &str = include_str!("CODE_OF_CONDUCT");
 const MAIL: &str = "[depta@engos.de](mailto:depta@engos.de)";
 
 pub fn get_code_of_conduct() -> String {

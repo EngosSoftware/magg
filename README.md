e9d59d759893a3e84f6690e50203edce1ec15b96
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-01-15 11:11:15 +0100

    Updates.

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
 ![build Linux][build-badge-linux]
 ![build Windows][build-badge-windows]
 ![build macOs][build-badge-macos]
-![build macOs arm64][build-badge-macos-arm64]
+![build macOs arm64][build-badge-macos-arm64]  
+[![mit-license][mit-badge]][mit-license-url]
+[![apache-license][apache-badge]][apache-license-url]
+[![cc][cc-badge]][cc-url]  
+[![mbh][mbh-badge]][mbh-url]
+[![es][es-badge]][es-url]
 
 [crates-badge]: https://img.shields.io/crates/v/magg.svg
 [crates-url]: https://crates.io/crates/magg
@@ -15,3 +20,35 @@
 [build-badge-windows]: https://github.com/EngosSoftware/magg/actions/workflows/build-windows.yml/badge.svg
 [build-badge-macos]: https://github.com/EngosSoftware/magg/actions/workflows/build-macos.yml/badge.svg
 [build-badge-macos-arm64]: https://github.com/EngosSoftware/magg/actions/workflows/build-macos-arm64.yml/badge.svg
+[mit-badge]: https://img.shields.io/badge/License-MIT-blue.svg
+[mit-url]: https://opensource.org/licenses/MIT
+[mit-license-url]: https://github.com/EngosSoftware/magg/blob/main/LICENSE-MIT
+[apache-badge]: https://img.shields.io/badge/License-Apache%202.0-blue.svg
+[apache-url]: https://www.apache.org/licenses/LICENSE-2.0
+[apache-license-url]: https://github.com/EngosSoftware/magg/blob/main/LICENSE
+[apache-notice-url]: https://github.com/EngosSoftware/magg/blob/main/NOTICE
+[cc-badge]: https://img.shields.io/badge/Contributor%20Covenant-2.1-blue.svg
+[cc-url]: https://github.com/EngosSoftware/magg/blob/main/CODE_OF_CONDUCT.md
+[mbh-badge]: https://img.shields.io/badge/Made_by-HUMAN-D81B60.svg
+[mbh-url]: https://github.com/DariuszDepta
+[es-badge]: https://img.shields.io/badge/Brought_to_you_by-Engos_Software-43A047.svg
+[es-url]: https://engos.de
+[repository-url]: https://github.com/EngosSoftware/magg
+
+
+
+
+## License
+
+Licensed under either of
+
+- [MIT license][mit-url] (see [LICENSE-MIT][mit-license-url]) or
+- [Apache License, Version 2.0][apache-url] (see [LICENSE][apache-license-url] and [NOTICE][apache-notice-url])
+
+at your option.
+
+## Contribution
+
+Any contributions to [magg][repository-url] are greatly appreciated.
+All contributions intentionally submitted for inclusion in the work by you,
+shall be dual licensed as above, without any additional terms or conditions.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::path::Path;
 
+const TWO_SPACES: &str = "  ";
+
 fn parse_toml(file_name: impl AsRef<Path>) -> toml::Value {
   let content = fs::read_to_string(file_name).expect("failed to read TOML file");
   toml::from_str(&content).expect("failed to parse TOML file")
@@ -17,15 +19,20 @@ fn get_repository(parsed: &toml::Value) -> &str {
 fn main() {
   let parsed_toml = parse_toml("Cargo.toml");
   let package_name = get_package_name(&parsed_toml);
-  let repository = get_repository(&parsed_toml).strip_suffix(".git").expect("repository name does not end with '.git'");
+  let repository = get_repository(&parsed_toml).strip_suffix(".git").expect("repository name does not end with '.git' suffix");
   println!("### {}", package_name);
   println!();
   println!("[![crates.io][crates-badge]][crates-url]");
-  println!("[![coverage][cov-badge]][cov-url]  ");
+  println!("[![coverage][cov-badge]][cov-url]{TWO_SPACES}");
   println!("![build Linux][build-badge-linux]");
   println!("![build Windows][build-badge-windows]");
   println!("![build macOs][build-badge-macos]");
-  println!("![build macOs arm64][build-badge-macos-arm64]");
+  println!("![build macOs arm64][build-badge-macos-arm64]{TWO_SPACES}");
+  println!("[![mit-license][mit-badge]][mit-license-url]");
+  println!("[![apache-license][apache-badge]][apache-license-url]");
+  println!("[![cc][cc-badge]][cc-url]{TWO_SPACES}");
+  println!("[![mbh][mbh-badge]][mbh-url]");
+  println!("[![es][es-badge]][es-url]");
   println!();
   println!("[crates-badge]: https://img.shields.io/crates/v/{package_name}.svg");
   println!("[crates-url]: https://crates.io/crates/{package_name}");
@@ -35,4 +42,28 @@ fn main() {
   println!("[build-badge-windows]: {repository}/actions/workflows/build-windows.yml/badge.svg");
   println!("[build-badge-macos]: {repository}/actions/workflows/build-macos.yml/badge.svg");
   println!("[build-badge-macos-arm64]: {repository}/actions/workflows/build-macos-arm64.yml/badge.svg");
+  println!("[mit-badge]: https://img.shields.io/badge/License-MIT-blue.svg");
+  println!("[mit-url]: https://opensource.org/licenses/MIT");
+  println!("[mit-license-url]: {repository}/blob/main/LICENSE-MIT");
+  println!("[apache-badge]: https://img.shields.io/badge/License-Apache%202.0-blue.svg");
+  println!("[apache-url]: https://www.apache.org/licenses/LICENSE-2.0");
+  println!("[apache-license-url]: {repository}/blob/main/LICENSE");
+  println!("[apache-notice-url]: {repository}/blob/main/NOTICE");
+  println!("[cc-badge]: https://img.shields.io/badge/Contributor%20Covenant-2.1-blue.svg");
+  println!("[cc-url]: {repository}/blob/main/CODE_OF_CONDUCT.md");
+  println!("[mbh-badge]: https://img.shields.io/badge/Made_by-HUMAN-D81B60.svg");
+  println!("[mbh-url]: https://github.com/DariuszDepta");
+  println!("[es-badge]: https://img.shields.io/badge/Brought_to_you_by-Engos_Software-43A047.svg");
+  println!("[es-url]: https://engos.de");
+  println!("[repository-url]: {repository}");
+  println!("\n\n\n");
+  println!("## License\n");
+  println!("Licensed under either of\n");
+  println!("- [MIT license][mit-url] (see [LICENSE-MIT][mit-license-url]) or");
+  println!("- [Apache License, Version 2.0][apache-url] (see [LICENSE][apache-license-url] and [NOTICE][apache-notice-url])\n");
+  println!("at your option.\n");
+  println!("## Contribution\n");
+  println!("Any contributions to [{package_name}][repository-url] are greatly appreciated.");
+  println!("All contributions intentionally submitted for inclusion in the work by you,");
+  println!("shall be dual licensed as above, without any additional terms or conditions.");
 }

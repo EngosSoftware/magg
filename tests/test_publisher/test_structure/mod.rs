b4c4c3f37eadc9cef2a1e09478929e102f5ae85a
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-01-26 10:07:21 +0100

    Updates.

--- tests/test_publisher/test_structure/mod.rs
+++ tests/test_publisher/test_structure/mod.rs
@@ -6,8 +6,8 @@ fn _0001() {
     .arg("-d")
     .arg("project_1")
     .arg("-f")
-    .arg("Cargø.toml")
-    .stdout("project_1/Cargø.toml\n")
+    .arg("Cargq.toml")
+    .stdout("project_1/Cargq.toml\n")
     .stderr("")
     .execute();
 }
